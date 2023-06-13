e66e1674559a7458747333c9a7ffb8ba7c8d023f
grif-on <91139654+grif-on@users.noreply.github.com>
2023-06-13 20:27:18 +0300

    core functional done

--- dump_tools.js
+++ dump_tools.js
@@ -26,7 +26,26 @@ SOFTWARE.
 //Intended for use in Tiled 1.8.6
 
 const strip_off_all_default_values = tiled.registerAction("Strip off all default values", function () {
-
+    let map = tiled.activeAsset;
+    for (let i = 0; i < map.layerCount; i++) {
+        current_layer = map.layerAt(i);
+        if (current_layer.isObjectLayer) {                          //игнорировать необъектные слои
+            if (current_layer.objects != null) {                    //на случай , если слой не будет иметь объектов вообще
+                current_layer.objects.forEach(function (processedObject) {
+                    let originalProperties = processedObject.properties();
+                    for (const [key, value] of Object.entries(originalProperties)) {
+                         tiled.log(typeof (value))
+                        if (typeof (value) === "object" && value.toString().charAt(0) == "#" && value.toString().length == 7) continue //skip colors objects
+                        processedObject.removeProperty(key); // remove property
+                        let defaultProperty = processedObject.resolvedProperty(key)
+                        if (defaultProperty !== value) {
+                            processedObject.setProperty(key, value); //return property back only if it has non-default value
+                        }
+                    }
+                });
+            }
+        }
+    }
 })
 
 strip_off_all_default_values.text = "Strip off all default values";
